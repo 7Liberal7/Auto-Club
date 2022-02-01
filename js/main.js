c6f6714c282b6f6af2432b6d641d1d3cb7957765
7Liberal7 <89695467+7Liberal7@users.noreply.github.com>
2022-02-02 01:27:44 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+  $('.home-slider__list').slick({
+    arrows: true,
+    dots: false,
+    slidesToShow: 4,
+    prevArrow: '<button class="slick-next slick__btn" type="button"></button>',
+    nextArrow: '<button class="slick-prev slick__btn" type="button"></button>',
+  });
