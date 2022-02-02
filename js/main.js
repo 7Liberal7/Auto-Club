2b180bf67a55966143d3abee9b52bbeedde2620a
7Liberal7 <89695467+7Liberal7@users.noreply.github.com>
2022-02-03 00:31:24 +0500

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-  $('.home-slider__list').slick({
-    arrows: true,
-    dots: false,
-    slidesToShow: 4,
-    prevArrow: '<button class="slick-next slick__btn" type="button"></button>',
-    nextArrow: '<button class="slick-prev slick__btn" type="button"></button>',
-  });
